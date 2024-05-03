553ee562011cb63b5ebdf57b1648c94ffb20c0fd
akeyo <akeyo@gmail.com>
2024-05-03 12:42:03 +0300

    final commit

--- app/src/main/java/com/akeyo/akeyoxfirebase/ui/theme/screens/login/LoginScreen.kt
+++ app/src/main/java/com/akeyo/akeyoxfirebase/ui/theme/screens/login/LoginScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 
@@ -28,7 +29,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-
+import com.akeyo.akeyoxfirebase.data.AuthViewModel
+import com.akeyo.akeyoxfirebase.navigation.ROUTE_REGISTER
 
 
 @Composable
